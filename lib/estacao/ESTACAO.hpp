cc93479240d46bdbaaa24cba406ad1a1b9451a29
joaoTrevizoli <joao@lab804.com.br>
2016-07-06 11:55:45 -0300

    first

---
+++ lib/estacao/ESTACAO.hpp
@@ -0,0 +1,21 @@
+
+// -----------------------------------------------//
+
+#ifndef ESTACAO_HPP
+#define  ESTACAO_HPP
+
+// -----------------------------------------------//
+
+#include "Arduino.h"
+#include <DHT.h>
+// -----------------------------------------------//
+
+class EstacaoConforto
+{
+public:
+  EstacaoConforto(const int pin);
+
+private:
+};
+
+#endif
