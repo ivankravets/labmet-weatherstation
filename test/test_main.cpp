e0bec4f13d4d017556f9dd4f43df89d0bf641463
joao <joao@lab804.com.br>
2016-12-07 14:13:52 -0200

    Unit testing bug report

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -12,11 +12,18 @@ void test_lcd_screen_addr(void);
 void test_bh1750_addr(void);
 void test_bmp180_addr(void);
 void test_rtc_addr(void);
+void test_rtc_addr_2(void);
 void test_ds18B20_begin (void);
-void test_led_builtin_pin_number(void);
-void test_led_state_high(void);
-void test_led_state_low(void);
+void test_ds18B20_improper_value(void);
 
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+
+}
 // -------------------------Defines------------------------------------------ //
 
 #define ONE_WIRE_BUS D3
@@ -28,30 +35,31 @@ DallasTemperature ds18b20Sensor(&oneWire);
 DS18b20 ds18b20(ds18b20Sensor, 500);
 // -------------------------------------------------------------------------- //
 
-void setup() {
-    Wire.begin();
-    ds18b20.begin();
-    pinMode(LED_BUILTIN, OUTPUT);
-    UNITY_BEGIN();
+void setup()
+{
+  Wire.begin();
+  ds18b20.begin();
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
+  UNITY_BEGIN();
+
 // -------------------------I2C Devices-------------------------------------- //
 
-    RUN_TEST(test_count_i2c_devices);
-    RUN_TEST(test_lcd_screen_addr);
-    RUN_TEST(test_bh1750_addr);
-    RUN_TEST(test_bmp180_addr);
-    RUN_TEST(test_rtc_addr);
+  RUN_TEST(test_count_i2c_devices);
+  RUN_TEST(test_lcd_screen_addr);
+  RUN_TEST(test_bh1750_addr);
+  RUN_TEST(test_bmp180_addr);
+  RUN_TEST(test_rtc_addr);
+  RUN_TEST(test_rtc_addr_2);
+
 // -------------------------Dallas Temperature------------------------------- //
 
-    RUN_TEST(test_ds18B20_begin);
-
-    RUN_TEST(test_led_builtin_pin_number);
-    RUN_TEST(test_led_state_high);
-    RUN_TEST(test_led_state_low);
-    UNITY_END();
+  RUN_TEST(test_ds18B20_begin);
+  RUN_TEST(test_ds18B20_improper_value);
+  UNITY_END();
 }
 
 // -------------------------Unittests---------------------------------------- //
@@ -79,30 +87,36 @@ void test_count_i2c_devices(void)
       nDevices++;
     }
   }
-  TEST_ASSERT_EQUAL(3, nDevices);
+  TEST_ASSERT_EQUAL(5, nDevices);
 }
 
 void test_lcd_screen_addr(void)
 {
-    byte error = wire_setup(0x27);
-    TEST_ASSERT_EQUAL(0, error);
+  byte error = wire_setup(0x27);
+  TEST_ASSERT_EQUAL(0, error);
 }
 
 void test_bh1750_addr(void)
 {
-    byte error = wire_setup(0x23);
-    TEST_ASSERT_EQUAL(0, error);
+  byte error = wire_setup(0x23);
+  TEST_ASSERT_EQUAL(0, error);
 }
 
 void test_bmp180_addr(void)
 {
-    byte error = wire_setup(0x77);
-    TEST_ASSERT_EQUAL(0, error);
+  byte error = wire_setup(0x77);
+  TEST_ASSERT_EQUAL(0, error);
 }
 
 void test_rtc_addr(void)
 {
   byte error = wire_setup(0x68);
+  TEST_ASSERT_EQUAL(0, error);
+}
+
+void test_rtc_addr_2(void)
+{
+  byte error = wire_setup(0x50);
   TEST_ASSERT_EQUAL(0, error);
 }
 
@@ -114,19 +128,11 @@ void test_ds18B20_begin (void)
   TEST_ASSERT_TRUE(test_soil_temp_sensor);
 }
 
-// -------------------------Generic Tests------------------------------------ //
-void test_led_builtin_pin_number(void)
+void test_ds18B20_improper_value(void)
 {
-    TEST_ASSERT_EQUAL(LED_BUILTIN, 16);
+  float temp = ds18b20.getTemperature();
+  TEST_ASSERT_FLOAT_WITHIN(100.0, 0.0, temp);
 }
-void test_led_state_high(void) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    TEST_ASSERT_EQUAL(digitalRead(LED_BUILTIN), HIGH);
-}
-
-void test_led_state_low(void) {
-    digitalWrite(LED_BUILTIN, LOW);
-    TEST_ASSERT_EQUAL(digitalRead(LED_BUILTIN), LOW);
-}
+// -------------------------Generic Tests------------------------------------ //
 
 #endif
