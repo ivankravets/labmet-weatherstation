00aa19eb1d8881ca779a57db27c2b2b7886b53b4
joao <joao@lab804.com.br>
2016-12-07 16:42:24 -0200

    bh1750 test ready

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -4,8 +4,10 @@
 #include <Wire.h>
 #include <Arduino.h>
 #include "DS18b20.hpp"
+#include "BH1750.hpp"
+#include "PressureSensor.hpp"
 
-
+//----------One Wire Devices-------- //
 byte wire_setup(byte addr);
 void test_count_i2c_devices(void);
 void test_lcd_screen_addr(void);
@@ -13,9 +15,16 @@ void test_bh1750_addr(void);
 void test_bmp180_addr(void);
 void test_rtc_addr(void);
 void test_rtc_addr_2(void);
+//----------Dallas Temperature----- //
 void test_ds18B20_begin (void);
 void test_ds18B20_improper_value(void);
+//----------BH1750----------------- //
+void test_bh1750_lum_raw_working(void);
+void test_bh1750_lum_working(void);
+//----------BMP180 Pressure-------- //
 
+
+//----------------------------------//
 void setUp(void)
 {
 }
@@ -33,12 +42,25 @@ void tearDown(void)
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature ds18b20Sensor(&oneWire);
 DS18b20 ds18b20(ds18b20Sensor, 500);
+
+// -------------------------BH1750------------------------------------------- //
+
+BH1750 bh1750;
+
+// ------------------------BMP180 Pressure----------------------------------- //
+
+SFE_BMP180 bmp180;
+PressureSensor pressureSensor(bmp180, 1000);
+
 // -------------------------------------------------------------------------- //
+
 
 void setup()
 {
   Wire.begin();
   ds18b20.begin();
+  bh1750.begin();
+  pressureSensor.begin();
   delay(1000);
   pinMode(LED_BUILTIN, OUTPUT);
   UNITY_BEGIN();
@@ -56,6 +78,15 @@ void setup()
 
   RUN_TEST(test_ds18B20_begin);
   RUN_TEST(test_ds18B20_improper_value);
+
+// ------------------------BH1750-------------------------------------------- //
+
+  RUN_TEST(test_bh1750_lum_raw_working);
+  RUN_TEST(test_bh1750_lum_working);
+
+// ------------------------BMP180 Pressure----------------------------------- //
+
+
   UNITY_END();
 }
 
@@ -134,6 +165,23 @@ void test_ds18B20_improper_value(void)
   float temp = ds18b20.getTemperature();
   TEST_ASSERT_FLOAT_WITHIN(100.0, 0.0, temp);
 }
+
+// ------------------------BH1750-------------------------------------------- //
+
+void test_bh1750_lum_raw_working(void)
+{
+  uint16_t luminance = bh1750.readRawValue();
+  TEST_ASSERT_NOT_EQUAL(65535, luminance);
+}
+
+void test_bh1750_lum_working(void)
+{
+  uint16_t luminance = bh1750.readIlluminanceLevel();
+  TEST_ASSERT_NOT_EQUAL(54612, luminance);
+}
+// ------------------------BMP180 Pressure----------------------------------- //
+
+
 // -------------------------Generic Tests------------------------------------ //
 
 #endif
